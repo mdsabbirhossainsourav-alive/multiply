bafacbc169af504df12f71da290dd1e5318e32cc
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2025-11-15 23:19:01 +0600

    Add files via upload

---
+++ Multiply.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    long long a;
+    long long b;
+    scanf("%lld",&a);
+    scanf("%lld",&b);
+    long long multiflication = a*b;
+    printf("%lld",multiflication);
+    return 0;
+}
